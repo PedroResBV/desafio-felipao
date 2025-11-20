0a66ad080bfba94425a5da488e559ac45ec517dd
PedroResBv <pedrohresende.santos@gmail.com>
2025-11-20 19:24:55 -0300

    Identificador simples de Ranking completo

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("olá mundão")
+let nomeHeroi = "Desterk"
+let xpHeroi = 3004
+let elo 
+
+if (xpHeroi < 1000){
+    elo = "Ferro"
+} else if (xpHeroi <= 2000){
+    elo = "Bronze"
+} else if (xpHeroi <= 5000){
+    elo = "Prata"
+} else if (xpHeroi <= 7000){
+    elo = "Ouro"
+} else if (xpHeroi <= 8000){
+    elo = "Platina"
+} else if (xpHeroi <= 9000){
+    elo = "Ascendente"
+} else if (xpHeroi <= 10000){
+    elo = "Imortal"
+} else {
+    elo = "Radiante"
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + elo )
